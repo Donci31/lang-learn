55fcf801e1050e6c0eeec8f04b66ef117869d189
Donci31 <82709410+Donci31@users.noreply.github.com>
2023-09-15 18:05:49 +0200

    Upgraded gradle groovy to gradle kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    }
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
